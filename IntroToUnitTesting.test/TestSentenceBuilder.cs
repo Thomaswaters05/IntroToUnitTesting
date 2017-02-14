454cb2540dceaa889a457628b5fe29feac676d98
Thomas Waters <Thomas.waters05@gmail.com>
2017-02-13 17:52:46 -0800

    pushing to github

---
+++ IntroToUnitTesting.test/TestSentenceBuilder.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntroToUnitTesting.test
+{
+    [TestClass]
+    public class TestSentenceBuilder
+    {
+        [TestMethod]
+        public void TestInstantiatingBuilder()
+        {
+            //Arrange
+            var builder = new SentenceBuilder();
+
+            //Assert
+            Assert.IsNotNull(builder);
+
+        }
+     
+        [TestMethod]
+        public void TestItGivesBackAString()
+        {
+            //Arrange
+            var builder = new SentenceBuilder();
+            string[] words = new string[] {"this" , "is", "a", "test"};
+
+            //Act
+           string output =  builder.BuildSentence(words);
+
+            //Assert
+            Assert.IsInstanceOfType(output, typeof(string));
+        }
+        [TestMethod]
+        public void TestBuildingASentence1()
+        {
+            //Arrange
+            var builder = new SentenceBuilder();
+            string[] words = new string[] { "This", "is", "a", "test" };
+
+            //Act
+            var output = builder.BuildSentence(words);
+
+            //Assert
+            string expectedOutput = "This is a test.";
+            Assert.AreEqual(expectedOutput, output);
+           
+        }
+        [TestMethod]
+        public void TestBuildingASentence2()
+        {
+            //Arrange
+            var builder = new SentenceBuilder();
+            string[] words = new string[] { "hello", "world", "from", "Moab" };
+
+            //Act
+            var output = builder.BuildSentence(words);
+
+            //Assert
+            string expectedOutput = "Hello world from Moab.";
+            Assert.AreEqual(expectedOutput, output);
+
+        }      
+    }
+}
---
+++ IntroToUnitTesting/SentenceBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToUnitTesting
+{
+    public class SentenceBuilder
+    {
+        public string BuildSentence(string[] inputWords)
+        {
+            
+            string sentence = "";
+
+           foreach (var word in inputWords)
+            {
+                sentence += word + " ";
+            }
+            sentence = sentence.Substring(0, sentence.Length - 1);
+            sentence += ".";
+            string finishedSentence = sentence[0].ToString().ToUpper() + sentence.Substring(1);
+
+            return finishedSentence;
+        }
+    }
+}
